bce24b0b791787e6e1a36aaf7c16920d4d593142
Igor Maf <knjaz092@mail.ru>
2015-04-24 21:14:09 +0300

    gulp settings, added sass and continue header

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 'use strict';
 
 var gulp = require('gulp'),
-	concatCSS = require('gulp-concat-css'),
 	minifyCSS = require('gulp-minify-css'),
 	rename = require('gulp-rename'),
-	notify = require('gulp-notify'),
 	livereload = require('gulp-livereload'),
 	connect = require('gulp-connect'),
-	autoprefixer = require('gulp-autoprefixer');
+	autoprefixer = require('gulp-autoprefixer'),
+	sass = require('gulp-sass'),
+	sourcemaps = require('gulp-sourcemaps');
 
 
 // server connect
@@ -18,6 +18,7 @@ gulp.task('connect', function() {
 	})
 });
 
+
 // html
 gulp.task('html', function() {
 	gulp.src('*.html')
@@ -25,35 +26,47 @@ gulp.task('html', function() {
 		.pipe(connect.reload());
 });
 
+
 // css
 gulp.task('css', function() {
-	gulp.src('css/**/*.css')
-		.pipe(concatCSS('style.css'))
+	gulp.src('scss/main.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass({
+			errLogToConsole: true
+		}))
+		.pipe(sourcemaps.write())
 		.pipe(autoprefixer())
 		.pipe(minifyCSS())
 		.pipe(rename('style.min.css'))
-		.pipe(gulp.dest('app/css'))
+		.pipe(gulp.dest('app/css/'))
 		.pipe(connect.reload());
-		//.pipe(notify('Done!'));
 });
+
 
 // images
 gulp.task('images', function() {
 	gulp.src('images/**')
-		.pipe(gulp.dest('app/images'))
+		.pipe(gulp.dest('app/images/'))
+		.pipe(connect.reload());
 });
+
 
 // fonts
 gulp.task('fonts', function() {
 	gulp.src('fonts/**')
 		.pipe(gulp.dest('app/fonts/'))
+		.pipe(connect.reload());
 });
+
 
 // watch
 gulp.task('watch', function() {
-	gulp.watch('css/**/*.css', ['css'])
+	gulp.watch('scss/**/*.scss', ['css'])
 	gulp.watch('*.html', ['html'])
+	gulp.watch('images/**', ['images'])
+	gulp.watch('fonts/**', ['fonts'])
 })
+
 
 // default
 gulp.task('default', ['connect', 'html', 'css', 'images', 'fonts', 'watch']);
